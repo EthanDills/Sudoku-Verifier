7b7092882d35bb510bf2924662f68f614cbaa447
Ethan Dills <sapientioraet@gmail.com>
2021-07-15 22:39:42 -0700

    create outline of code

---
+++ sudoku.py
@@ -0,0 +1,11 @@
+## given a 2D array of a solvable sudoku map (9 by 9 grid), solve the sudoku and output the solved sudoku map##
+
+##
+# Input 2D Array
+    # sudoku = [[],[], ...]
+# Compute Sudoku solution
+    # 
+# Output solved sudoku
+    # print(sudoku solution)
+# 
+# ##
